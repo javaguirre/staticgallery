db8fa7fc845c20bb888a32aded61f42ac58a5566
Javier Aguirre <contacto@javaguirre.net>
2012-08-06 22:57:10 +0200

    Adding a global generator, some refactoring also

--- app.py
+++ app.py
@@ -6,24 +6,18 @@ Small web service for static gallery generation.
 import flask
 from flask import Flask
 from flask import render_template
-
 from PIL import Image
-
 import os
 
 app = Flask(__name__)
+app.config.from_pyfile("configuration.py")
 
-here_folder = os.getcwd()
-
-static_name = 'static'
-gallery_name = 'gallery'
-thumbs_name = 'thumbs'
-readme_name = 'README'
-index_gallery_name = 'index.html'
-secret_prefix_name = 'secret_'
-
-index_title = 'Title of the gallery'
-index_image_name = 'logo.jpg'
+#FIXME Provisional
+static_name = app.config['STATIC_NAME']
+gallery_name = app.config['GALLERY_NAME']
+thumbs_name = app.config['THUMBS_NAME']
+index_gallery_name = app.config['INDEX_GALLERY_NAME']
+secret_prefix_name = app.config['SECRET_PREFIX_NAME']
 
 # url parse is ugly
 static_url = '/%s/'
@@ -59,8 +53,8 @@ def get_gallery_path(folder):
 
 	"""
 
-	gallery_path = os.path.join(here_folder, gallery_base_path % (static_name, gallery_name, folder))
-	thumbs_path = os.path.join(here_folder, thumbs_base_path % (static_name, gallery_name, folder, thumbs_name))
+	gallery_path = os.path.join(app.config['ACTUAL_PATH'], gallery_base_path % (static_name, gallery_name, folder))
+	thumbs_path = os.path.join(app.config['ACTUAL_PATH'], thumbs_base_path % (static_name, gallery_name, folder, thumbs_name))
 
 	return gallery_path, thumbs_path
 
@@ -71,78 +65,65 @@ def do_menu():
 
 	"""
 
-	index_image_url = (gallery_base_url % (static_name, gallery_name, index_image_name))[:-1]
-
-	static_path =os.path.normcase(os.path.join(here_folder, static_base_path % (static_name, gallery_name)))
-
+	index_image_url = (gallery_base_url % (static_name, gallery_name, app.config['INDEX_IMAGE_NAME']))[:-1]
+	static_path =os.path.normcase(os.path.join(app.config['ACTUAL_PATH'], static_base_path % (static_name, gallery_name)))
 	galleries = os.listdir(static_path)
-
 
 	items = []
 
 	for gallery in galleries:
 		# check each gallery
-		if gallery.startswith(secret_prefix_name) == False and gallery != index_image_name:
+		if gallery.startswith(secret_prefix_name) == False and gallery != app.config['INDEX_IMAGE_NAME']:
 			# do not display secret one
 			gallery_url, thumbs_url = get_gallery_urls(gallery)
 			items.append(Item(gallery, gallery_url))
 
-	return render_template('menu.jinja2', title=index_title, items=items, image_url=index_image_url)
+	return render_template('menu.jinja2', title=app.config['INDEX_TITLE'], items=items, image_url=index_image_url)
 
-@app.route('/do/gallery/<folder>/')
-def do_gallery(folder):
-	"""
-	Compute a gallery creating static file from a template
+@app.route('/do_gallery/')
+def do_gallery():
+    for elem in os.listdir(os.path.join(app.config['ACTUAL_PATH'], static_name, gallery_name)):
+        if os.path.isdir(os.path.join(app.config['ACTUAL_PATH'], static_name, gallery_name, elem)):
+            gallery_path, thumbs_path = get_gallery_path(elem)
+            image_list = os.listdir(gallery_path)
 
-	"""
+            if thumbs_name in image_list:
+                # suppress thumbnail folder
+                image_list.remove(thumbs_name)
 
-	# process all pictures
-	gallery_path, thumbs_path = get_gallery_path(folder)
-	image_list = os.listdir(gallery_path)
+            if index_gallery_name in image_list:
+                # simply remove
+                image_list.remove(index_gallery_name)
 
-	if thumbs_name in image_list:
-		# suppress thumbnail folder
-		image_list.remove(thumbs_name)
+            if os.path.exists(thumbs_path) == False:
+                # create thumbs directory
+                os.mkdir(thumbs_path)
 
-	if readme_name in image_list:
-		# apply readme
-		image_list.remove(readme_name)
+            image_list.sort()
 
-		readme = open(gallery_path + readme_name, 'r').read()
-	else:
-		readme = 'No comment available !'
+            for image in image_list:
+                # create thumbs
+                image_path = os.path.join(gallery_path, image)
+                image_thumb_path = os.path.join(thumbs_path, image)
 
-	if index_gallery_name in image_list:
-		# simply remove
-		image_list.remove(index_gallery_name)
+                image_full = Image.open(os.path.normcase(image_path))
+                image_full.thumbnail(thumbs_size, Image.ANTIALIAS)
+                image_full.save(image_thumb_path, image_full.format)
 
-	if os.path.exists(thumbs_path) == False:
-		# create thumbs directory
-		os.mkdir(thumbs_path)
+            # generate template
+            gallery_url, thumbs_url = get_gallery_urls(elem)
+            template = render_template('base.jinja2', title=elem, thumbs_url=thumbs_url, gallery_url=gallery_url, image_list=image_list)
 
-	image_list.sort()
+            # save static file
+            new_file = file(os.path.join(gallery_path, index_gallery_name), 'w')
+            new_file.write(template)
+            new_file.close()
 
-	for image in image_list:
-		# create thumbs
-		image_path = os.path.join(gallery_path, image)
-		image_thumb_path = os.path.join(thumbs_path, image)
+    # FIXME Provisional
+    gallery_url = "index.html"
 
-		image_full = Image.open(os.path.normcase(image_path))
-		image_full.thumbnail(thumbs_size, Image.ANTIALIAS)
-   		image_full.save(image_thumb_path, image_full.format)
-
-	# generate template
-	gallery_url, thumbs_url = get_gallery_urls(folder)
-	template = render_template('base.jinja2', title=folder, thumbs_url=thumbs_url, gallery_url=gallery_url, image_list=image_list, readme=readme)
-
-	# save static file
-	new_file = file(os.path.join(gallery_path, index_gallery_name), 'w')
-	new_file.write(template)
-	new_file.close()
-
-	return flask.redirect(gallery_url + index_gallery_name, code=302)
+    return flask.redirect(gallery_url + index_gallery_name, code=302)
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run(host='0.0.0.0', port=5000)
-		
+    app.run(host='0.0.0.0', port=5000)
+
---
+++ configuration.py
@@ -0,0 +1,13 @@
+import os
+
+DEBUG = True
+ACTUAL_PATH = os.path.dirname(__file__)
+STATIC_NAME = 'static'
+GALLERY_NAME = 'gallery'
+THUMBS_NAME = 'thumbs'
+README_NAME = 'readme'
+INDEX_GALLERY_NAME = 'index.html'
+SECRET_PREFIX_NAME = 'secret_'
+
+INDEX_TITLE = 'Title of the gallery'
+INDEX_IMAGE_NAME = 'logo.jpg'
