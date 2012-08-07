ad4b949a91fcdb122a41da712e91baaa736e9613
Javier Aguirre <contacto@javaguirre.net>
2012-08-07 23:26:10 +0200

    Adding exiftags to get the orientation of the photo, under construction

--- generator.py
+++ generator.py
@@ -4,6 +4,7 @@
 """
 
 from PIL import Image
+import ExifTags
 import os
 from jinja2 import Environment, PackageLoader
 from configuration import *
@@ -35,16 +36,17 @@ def create_menu():
                 galleries.append({"title": gallery, "url": "/".join(["/" + gallery, "index.html"])})
 
         generate_html_output("/".join([gallery_path, "index.html"]), "menu.jinja2",
-                             {"title": "Menu", "galleries": galleries})
+                             {"title": "Album", "galleries": galleries})
 
 
 def create_gallery():
     gallery_path = os.path.join(ACTUAL_PATH, GALLERY_NAME)
 
-    for elem in os.listdir(gallery_path):
-        gallery_elem_path = os.path.join(gallery_path, elem)
+    for gallery_name in os.listdir(gallery_path):
+        gallery_elem_path = os.path.join(gallery_path, gallery_name)
 
-        if os.path.isdir(gallery_elem_path) and elem != "static":
+        if os.path.isdir(gallery_elem_path) and gallery_name != "static":
+            # TODO if file is already in the index, skip process
             thumbs_path = os.path.join(gallery_elem_path, THUMBS_NAME)
             image_list = os.listdir(gallery_elem_path)
 
@@ -65,14 +67,28 @@ def create_gallery():
                 image_thumb_path = os.path.join(thumbs_path, image)
 
                 image_full = Image.open(os.path.normcase(image_path))
+                #exif=dict((ExifTags.TAGS[k], v) for k, v in image_full._getexif().items())
+                exif = {}
+
+                for k, v in image_full._getexif().items():
+                    # TODO Get orientation and exit
+                    try:
+                        exif[ExifTags.TAGS[k]] = v
+                    except KeyError:
+                        pass
+
+                if not exif['Orientation'] or exif['Orientation'] == 6:
+                    image_full=image_full.rotate(-90, expand=True)
+
                 image_full.thumbnail(THUMBS_SIZE, Image.ANTIALIAS)
                 image_full.save(image_thumb_path, image_full.format)
 
-            gallery_url = "".join(["/",  elem, "/"])
+
+            gallery_url = "".join(["/",  gallery_name, "/"])
             thumbs_url = "".join([gallery_url, THUMBS_NAME, "/"])
             generate_html_output("/".join([gallery_elem_path, "index.html"]), "base.jinja2",
-                                 {"title": elem, "thumbs_url": thumbs_url, "gallery_url": gallery_url, "image_list": image_list})
+                                 {"title": gallery_name.capitalize(), "thumbs_url": thumbs_url, "gallery_url": gallery_url, "image_list": image_list})
 
 if __name__ == '__main__':
-    create_gallery()
     create_menu()
+    create_gallery()
