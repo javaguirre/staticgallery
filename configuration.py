09b006ca4d3bc557bc84e1f653c38663925f3798
Javier Aguirre <contacto@javaguirre.net>
2012-08-07 21:49:32 +0200

    Refactoring and creating menu index

--- configuration.py
+++ configuration.py
@@ -5,7 +5,6 @@ ACTUAL_PATH = os.path.dirname(__file__)
 GALLERY_NAME = 'gallery'
 THUMBS_NAME = 'thumbs'
 README_NAME = 'readme'
-INDEX_GALLERY_NAME = 'index.html'
 SECRET_PREFIX_NAME = 'secret_'
 
 INDEX_TITLE = 'Title of the gallery'
--- generator.py
+++ generator.py
@@ -14,6 +14,16 @@ sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir))
 env = Environment(loader=PackageLoader('static_gallery_generator', 'templates'))
 
 
+def generate_html_output(path, template_name, args):
+    template = env.get_template(template_name)
+    template_content = template.render(**args)
+
+    # save static file
+    new_file = file(path, 'w')
+    new_file.write(template_content)
+    new_file.close()
+
+
 def create_menu():
         galleries = []
         gallery_path = os.path.join(ACTUAL_PATH, GALLERY_NAME)
@@ -21,17 +31,11 @@ def create_menu():
         for gallery in os.listdir(gallery_path):
             gallery_elem_path = os.path.join(gallery_path, gallery)
 
-            if os.path.isdir(gallery_elem_path):
-                galleries.append({"title": gallery, "url": "/".join(["/" + GALLERY_NAME, gallery, "index.html"])})
+            if os.path.isdir(gallery_elem_path) and gallery != "static":
+                galleries.append({"title": gallery, "url": "/".join(["/" + gallery, "index.html"])})
 
-        template = env.get_template('menu.jinja2')
-        template_content = template.render(title="MENU", galleries=galleries)
-
-        # save static file
-        new_file = file("/".join([gallery_path, "index.html"]), 'w')
-        new_file.write(template_content)
-        new_file.close()
-
+        generate_html_output("/".join([gallery_path, "index.html"]), "menu.jinja2",
+                             {"title": "Menu", "galleries": galleries})
 
 
 def create_gallery():
@@ -39,17 +43,13 @@ def create_gallery():
 
     for elem in os.listdir(gallery_path):
         gallery_elem_path = os.path.join(gallery_path, elem)
-        if os.path.isdir(gallery_elem_path):
+
+        if os.path.isdir(gallery_elem_path) and elem != "static":
             thumbs_path = os.path.join(gallery_elem_path, THUMBS_NAME)
             image_list = os.listdir(gallery_elem_path)
 
             if THUMBS_NAME in image_list:
-                # suppress thumbnail folder
                 image_list.remove(THUMBS_NAME)
-
-            if INDEX_GALLERY_NAME in image_list:
-                # simply remove
-                image_list.remove(INDEX_GALLERY_NAME)
 
             if not os.path.exists(thumbs_path):
                 os.mkdir(thumbs_path)
@@ -65,16 +65,11 @@ def create_gallery():
                 image_full.thumbnail(THUMBS_SIZE, Image.ANTIALIAS)
                 image_full.save(image_thumb_path, image_full.format)
 
-            gallery_url = "/".join(["/" + GALLERY_NAME, elem + "/"])
+            gallery_url = "".join(["/",  elem, "/"])
             thumbs_url = "".join([gallery_url, THUMBS_NAME, "/"])
-            template = env.get_template('base.jinja2')
-            template_content = template.render(title=elem, thumbs_url=thumbs_url, gallery_url=gallery_url, image_list=image_list)
-
-            # save static file
-            new_file = file("/".join([gallery_elem_path, "index.html"]), 'w')
-            new_file.write(template_content)
-            new_file.close()
+            generate_html_output("/".join([gallery_elem_path, "index.html"]), "base.jinja2",
+                                 {"title": elem, "thumbs_url": thumbs_url, "gallery_url": gallery_url, "image_list": image_list})
 
 if __name__ == '__main__':
-    #create_gallery()
+    create_gallery()
     create_menu()
